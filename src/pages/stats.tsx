226f42bf987f1044cf2d175b4a848e06ca471678
Adriel Benjamin Martinez Quintanilla <adriel.martinez@kindercare.com>
2024-03-26 15:32:22 -0600

    Feat: Building stats page, Html structure and CSS

---
+++ src/pages/stats.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Stats = () => {
+  return (
+    <div className="stats-list-container">
+        <h1 className="stats-list-title" >Mis compromisos:</h1>
+        <ol className="stats-list" >
+            <li>1- Crear contenido <span>⭐</span> </li>        
+            <li>2- Aprender Sitecore <span>⭐</span> </li>
+            <li>3- Hacer ejercicios <span>⭐</span> </li>
+            <li>4- Ser más proactivo <span>⭐</span> </li>
+            <li>5- ser independiente <span>⭐</span> </li>
+        </ol>
+        
+    </div>
+    
+  )
+}
+
+export default Stats
